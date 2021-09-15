b5d6b3e456edc2f44ef024ae870e80cc7f84adb3
Kayrat <makhmudov.kay@gmail.com>
2021-09-15 22:06:48 +0500

    create Card, Drawer and Header component & fix style

--- src/App.js
+++ src/App.js
@@ -1,75 +1,23 @@
+import Card from "./components/Card";
+import Drawer from "./components/Drawer";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="wrapper clear">
-      <header className="d-flex justify-between align-center p-40">
-        <div className="d-flex align-center">
-          <img className="mr-15" width={40} height={40} src="/img/logo.png" alt="Logo" />
-          <div>
-            <h3 className="text-uppercase">React Sneakers</h3>
-            <p className="opacity-5">Магазин лучших кросовок</p>
-          </div>
-        </div>
-          <ul className="d-flex">
-            <li className="mr-30 d-flex align-center">
-              <img width={20} className="mr-10" height={20} src="/img/cart.svg" alt="cart" />
-              <span>1205 руб.</span>
-            </li>
-            <li className="d-flex align-center">
-              <img width={20} height={20} src="/img/user.svg" alt="user" />
-            </li>
-          </ul>
-      </header>
+    <div className="wrapper clear">      
+      <Drawer />
+      <Header />
       <div className="content p-40">
-        <h1 className="mb-40">Все кроссовки</h1>
-        
+        <div className="mb-40 d-flex align-center justify-between">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>        
         <div className="d-flex">
-          <div className="card">
-            <img width={133} height={120} src="/img/sneakers/1.jpg" alt="sneakers" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Цена:</span>
-                <b>12 990 руб.</b>
-              </div>
-              <button className="button"><img width={11} height={11} src="/img/plus.svg" alt="Plus" /></button>
-            </div>
-          </div>
-          <div className="card">
-            <img width={133} height={120} src="/img/sneakers/2.jpg" alt="sneakers" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Цена:</span>
-                <b>12 990 руб.</b>
-              </div>
-              <button className="button"><img width={11} height={11} src="/img/plus.svg" alt="Plus" /></button>
-            </div>
-          </div>
-          <div className="card">
-            <img width={133} height={120} src="/img/sneakers/3.jpg" alt="sneakers" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Цена:</span>
-                <b>12 990 руб.</b>
-              </div>
-              <button className="button"><img width={11} height={11} src="/img/plus.svg" alt="Plus" /></button>
-            </div>
-          </div>
-          <div className="card">
-            <img width={133} height={120} src="/img/sneakers/4.jpg" alt="sneakers" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Цена:</span>
-                <b>12 990 руб.</b>
-              </div>
-              <button className="button"><img width={11} height={11} src="/img/plus.svg" alt="Plus" /></button>
-            </div>
-          </div>
+          <Card />
         </div>
-
       </div>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="d-flex justify-between align-center p-40">
+            <div className="d-flex align-center">
+                <img className="mr-15" width={40} height={40} src="/img/logo.png" alt="Logo" />
+                <div>
+                    <h3 className="text-uppercase">React Sneakers</h3>
+                    <p className="opacity-5">Магазин лучших кросовок</p>
+                </div>
+            </div>
+            <ul className="d-flex">
+                <li className="mr-30 d-flex align-center">
+                    <img width={20} className="mr-10" height={20} src="/img/cart.svg" alt="cart" />
+                    <span>1205 руб.</span>
+                </li>
+                <li className="d-flex align-center">
+                    <img width={20} height={20} src="/img/user.svg" alt="user" />
+                </li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
